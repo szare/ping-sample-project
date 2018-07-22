e1feb65864da6429226fa619a8728e4b468734f6
Salman Zare <salman.zare@karteldigital.com>
2018-07-23 08:35:30 +1000

    adding cllassName

--- src/features/pages/About.tsx
+++ src/features/pages/About.tsx
@@ -3,9 +3,9 @@ import * as React from 'react';
 class About extends React.Component {
     public render() {
         return (
-            <div>
+            <>
                 about page....
-            </div>
+            </>
         );
     }
 }
